c9f7030e8950f747f7165e7c104f12fc18d072c0
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-06 09:40:52 +0300

    Implementing add contact php api

---
+++ backend/app/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function addOrUpdateContact(Request $request, $id = "add"){
+        if($id == "add"){
+            $contact = new Contact;
+        }else{
+            $contact = Contact::find($id);
+        }
+
+        $contact->id = $request-> id;
+        $contact->name = $request->name ? $request->name : $contact->name;
+        $contact->phone_number = $request->phone_number ? $request->phone_number : $contact->phone_number;
+        $contact->latitude = $request->latitude ? $request->latitude : $contact->latitude;
+        $contact->longitude = $request->longitude ? $request->longitude : $contact->longitude;
+        $contact->save();
+
+        return response(["contacts" => $contact]);
+
+    }
+}
