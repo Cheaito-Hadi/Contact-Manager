f2932e9513f76529ac656e098ad15d91b1d9ea65
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-06 08:39:24 +0300

    Adding php files

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number')->unique();
+            $table->string('latitude');
+            $table->string('longitude');
             $table->timestamps();
         });
     }
