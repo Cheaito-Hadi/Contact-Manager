98b7d03dd416822ea5f9f7755769addf38cb8c77
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-06 17:58:55 +0300

    Fetching contacts from api

--- frontend/src/Components/Cards/Cards.js
+++ frontend/src/Components/Cards/Cards.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import "./Cards.css";
 import { useEffect, useState } from "react";
 
-
 export default function Cards() {
   const [cards, setCards] = useState([]);
 
-  const fetchCards = async ()=>{
-    const response = await axios.get("http://localhost:3001/posts");
-    setPosts(response.data)
-  }
-  useEffect(()=>{
-    fetchPosts()
-  }, [])
-
+  const fetchCards = async () => {
+    const response = await axios.get("http://127.0.0.1:8000/api/show_contacts");
+    setCards(response.data.contacts);
+  };
+  useEffect(() => {
+    fetchCards();
+  }, []);
 
   return (
-      <div className="card">
-        <p>Name: </p>
-        <p>Phone number: </p>
-        <p>Adress: </p>
-      </div>
+    <div>
+      {cards.map((card) => {
+        return(
+        <div className="card">
+          <p>Name: {card.name}</p>
+          <p>Phone number: {card.phone_number}</p>
+          <p>Adress: {card.latitude}-{card.longitude}</p>
+        </div>
+      )})}
+    </div>
   );
 }
